9402e4dc79e9488f0f714e2650d4dac664a5cc2a
Ameth Ordoñez <amethgabriel@hotmail.com>
2022-10-14 14:16:57 -0500

    Server Ip add

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const ip = 'http://localhost';
 
 app.get('/', (request, response) => {
   response.send('Hola mi servidro en Express');
 });
 
 app.listen(port, () => {
-  console.log('Servidor corriendo en el puerto ' + port);
+  console.log('Servidor corriendo en ' + ip + ':' + port);
 });
